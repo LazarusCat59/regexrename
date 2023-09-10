4b810b8ed9fd083074fc3f69ae59304f7c9d150f
LazarusCat59 <zayedferosh@gmail.com>
2023-09-10 14:17:22 +0530

    Added conditions to normalize padding value

--- src/stringfuncs.c
+++ src/stringfuncs.c
@@ -9,15 +9,15 @@
 
 
 /******************************************************************************
-* Function:         char *join_path
-* Description:      Concatenates 2 paths, adding the seperator '/' if
-*                   necessary
-* Where:
-*                   char path1[] - Beginning of path
-*                   char path2[] - Ending of path
-* Return:           Pointer to concatenated path, has to be freed
-* Error:            Not expectin this to fail rn
-*****************************************************************************/
+ * Function:         char *join_path
+ * Description:      Concatenates 2 paths, adding the seperator '/' if
+ *                   necessary
+ * Where:
+ *                   char path1[] - Beginning of path
+ *                   char path2[] - Ending of path
+ * Return:           Pointer to concatenated path, has to be freed
+ * Error:            Not expectin this to fail rn
+ *****************************************************************************/
 char *join_path(char path1[], char path2[])
 {
     char *newpath = calloc(PATH_MAX, sizeof(char));
@@ -31,18 +31,18 @@ char *join_path(char path1[], char path2[])
 }
 
 /******************************************************************************
-* Function:         int check_nums
-* Description:      Gets value after the $ and checks if there is padding
-*                   value after it. If there is, padding value is stored 
-*                   to appropriate location in struct rename_data
-* Where:
-*                   struct rename_data *rat - for storing padding data,
-*                       should already be allocated, except for substrings
-*                   char *str - pointer to character after "$"
-*                   int len - length until end of str from pointer char
-* Return:           Integer value after $ if the pattern is valid
-* Error:            Negative values on error
-*****************************************************************************/
+ * Function:         int check_nums
+ * Description:      Gets value after the $ and checks if there is padding
+ *                   value after it. If there is, padding value is stored 
+ *                   to appropriate location in struct rename_data
+ * Where:
+ *                   struct rename_data *rat - for storing padding data,
+ *                       should already be allocated, except for substrings
+ *                   char *str - pointer to character after "$"
+ *                   int len - length until end of str from pointer char
+ * Return:           Integer value after $ if the pattern is valid
+ * Error:            Negative values on error
+ *****************************************************************************/
 int check_nums(struct rename_data *rat, char *str, int len)
 {
     int i, retval = -1;
@@ -76,47 +76,50 @@ int check_nums(struct rename_data *rat, char *str, int len)
 }
 
 /******************************************************************************
-* Function:         int get_rename_data
-* Description:      Parses the substitute string and stores values to be 
-*                   substituted in struct rename_data, with padding if it's
-*                   an integer value. If it's not an integer value, padding
-*                   will be -1
-* Where:
-*                   struct captured_data *cat - To get substrings to store
-*                       in struct rename_data
-*                   struct rename_data *rat - Stores substrings in this
-*                       struct, initializes padding values
-*                   char *str - Substitution pattern (eg. $2 $1{3})
-* Return:           0 if there were no errors 
-* Error:            -1 if there was an error
-*****************************************************************************/
-
+ * Function:         int get_rename_data
+ * Description:      Parses the substitute string and stores values to be 
+ *                   substituted in struct rename_data, with padding if it's
+ *                   an integer value. If it's not an integer value, padding
+ *                   will be -1
+ * Where:
+ *                   struct captured_data *cat - To get substrings to store
+ *                       in struct rename_data
+ *                   struct rename_data *rat - Stores substrings in this
+ *                       struct, initializes padding values
+ *                   char *str - Substitution pattern (eg. $2 $1{3})
+ * Return:           0 if there were no errors 
+ * Error:            -1 if there was an error
+ *****************************************************************************/
 int get_rename_data(struct captured_data *cat, struct rename_data *rat, char *str)
 {
     int i, rc, len = strlen(str);
+    char *temp;
 
     for(i = 0; i < len; i++) {
-        switch(str[i]) {
-            case '$':
-                rc = check_nums(rat, str+i+1, len);
-                if(rc == -1) {
-                    printf("No closing braces or padding value > 9.\n");
-                    return 1;
-                } else if(rc == -2) {
-                    printf("Cannot have more than 99 capture groups.\n");
-                    return 1;
-                } else if(rc >= (int) cat->size) {
-                    printf("Variable num higher than no. of captures\n");
-                    return 1;
-                } else {
-                    rc--;
-                    rat->substrings[rc] = strdup(cat->substrings[rc+1]);
-                    printf("%d: %s, padding: %d\n", rc+1, rat->substrings[rc], rat->padding[rc]);
+        if(str[i] == '$') {
+            temp = NULL;
+            rc = check_nums(rat, str+i+1, len);
+            if(rc == -1) {
+                printf("No closing braces or padding value > 9.\n");
+                return 1;
+            } else if(rc == -2) {
+                printf("Cannot have more than 99 capture groups.\n");
+                return 1;
+            } else if(rc >= (int) cat->size) {
+                printf("Variable num higher than no. of captures\n");
+                return 1;
+            } else {
+                rc--;
+                rat->substrings[rc] = strdup(cat->substrings[rc+1]);
+                (void)strtol(rat->substrings[rc], &temp, 10);
+                if(temp[0] != '\0' && rat->padding[rc] > 0) {
+                    printf("Padding not allowed for strings, no padding will be applied.\n");
+                    rat->padding[rc] = -1;
+                } else if(temp[0] == '\0' && rat->padding[rc] == -1) {
+                    rat->padding[rc] = 0;
                 }
-                break;
-            default:
-                continue;
-                break;
+                printf("%d: %s, padding: %d\n", rc+1, rat->substrings[rc], rat->padding[rc]);
+            }
         }
     }
 
