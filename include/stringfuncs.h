59fe35f2408a7149f6b94c12302a7c7326fd0ed8
LazarusCat59 <zayedferosh@gmail.com>
2023-10-08 15:24:35 +0530

    Changed function name to set_refstr

--- include/stringfuncs.h
+++ include/stringfuncs.h
@@ -3,6 +3,6 @@
 
 char *join_path(char [], char []);
 
-int get_rename_data(struct captured_data *, struct rename_data *, char *);
+int set_refstr(struct captured_data *, struct rename_data *, char *);
 
 #endif
